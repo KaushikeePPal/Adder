473167cd535996b4f7a815f61dfa5844b3004b37
KaushikeePPal <kaushikee@gmail.com>
2021-01-05 18:28:59 +0530

    This is Maven Java Project for Adder

---
+++ Adder/src/main/java/Subractor.java
@@ -0,0 +1,11 @@
+
+public class Subractor {
+	
+	public static void main(String[] args) {
+        
+	      int num1 = 5, num2 = 15,  subtract;	      
+          subtract = num2 - num1;	     
+	      System.out.println("Difference of these numbers: "+subtract);
+	   }
+
+}
